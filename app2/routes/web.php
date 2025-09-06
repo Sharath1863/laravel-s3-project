879fb793bc156c16d3a5687d6d62b81e547d72fa
Sharath1863 <sharathraghav2003@gmail.com>
2025-09-06 10:49:02 +0530

    Update root route to redirect to upload page

--- app2/routes/web.php
+++ app2/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/upload');
 });
 
 Route::get('/upload', [UploadController::class, 'index'])->name('upload.form');
